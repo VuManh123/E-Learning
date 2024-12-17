6d127548c664feea8b4eda1444397c4aaa808724
HoangLong <hoanghafnooi1234@gmail.com>
2024-12-17 22:28:26 +0700

    update

---
+++ adminPage/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
+import ContentPage from './pages/ContentPage';
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/content" element={<ContentPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
